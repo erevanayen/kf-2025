b9c6f24e46ffffb09d43ee234fade32901381b59
erevanayen <zed.durotan@gmail.com>
2025-01-12 13:55:27 +0100

    chore: gh pages deploy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/kf-25",
+  assetPrefix: "/kf-25",
 };
 
 export default nextConfig;
